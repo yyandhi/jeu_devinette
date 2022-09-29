9c193c4570f2dc250b23514a68609e0c2a2a2da9
wanj <wanj@educ.college-montreal.qc.ca>
2022-09-29 10:43:47 -0400

    Jeu devinette

--- jeu_devinette.py
+++ jeu_devinette.py
@@ -1,3 +1,31 @@
-import random
+# Programme de jeu de devinette entre 0 à 100
 
-random.
+from random import *
+nb_essaie = 0
+
+jouer = True
+
+while jouer:
+    random_number = randint(0, 100)
+
+    not_found = True
+    while not_found:
+
+        print(random_number)
+        user_guess = int(input("Quelle numéro devinez-vous?\n"))
+
+        if user_guess < random_number:
+            print("Vôtre numéro est trop petit")
+            nb_essaie += 1
+
+        elif user_guess > random_number:
+            print("Vôtre numéro est trop grand.")
+            nb_essaie += 1
+
+        elif user_guess == random_number:
+            print(f"Bravo! Vous avez devinez le numéro en {nb_essaie} essaies.")
+            rejouer = input("Voulez-vous rejouer? (y/n)")
+            if rejouer == "y":
+                pass
+            elif rejouer == "n":
+                break
