8042a97b51e819a369cf1416954e076c6c7fb248
wanj <wanj@educ.college-montreal.qc.ca>
2022-10-13 09:49:09 -0400

    Test

--- jeu_devinette.py
+++ jeu_devinette.py
@@ -1,31 +1,41 @@
 # Programme de jeu de devinette entre 0 à 100
+#allo
 
 from random import *
-nb_essaie = 0
 
 jouer = True
 
 while jouer:
     random_number = randint(0, 100)
+    nb_essaie = 1
 
     not_found = True
     while not_found:
 
-        print(random_number)
-        user_guess = int(input("Quelle numéro devinez-vous?\n"))
+        try:
+            user_guess = int(input("Quelle numéro devinez-vous?\n"))
 
-        if user_guess < random_number:
-            print("Vôtre numéro est trop petit")
-            nb_essaie += 1
+        except:
+            print("Veuillez ecrire un nombre s'il vous plaît.")
 
-        elif user_guess > random_number:
-            print("Vôtre numéro est trop grand.")
-            nb_essaie += 1
+        else:
 
-        elif user_guess == random_number:
-            print(f"Bravo! Vous avez devinez le numéro en {nb_essaie} essaies.")
-            rejouer = input("Voulez-vous rejouer? (y/n)")
-            if rejouer == "y":
-                pass
-            elif rejouer == "n":
-                break
+            if user_guess < random_number:
+                print("Vôtre numéro est trop petit")
+                nb_essaie += 1
+
+            elif user_guess > random_number:
+                print("Vôtre numéro est trop grand.")
+                nb_essaie += 1
+
+            elif user_guess == random_number:
+                print(f"Bravo! Vous avez devinez le numéro en {nb_essaie} essaies.")
+                rejouer = input("Voulez-vous rejouer? (y/n)")
+                not_found = False
+
+                if rejouer == "y":
+                    jouer = True
+
+                elif rejouer == "n":
+                    print("Au revoir")
+                    jouer = False
