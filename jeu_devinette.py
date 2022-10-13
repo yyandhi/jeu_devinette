42e74102a0df979e0ac148e9b8033dbbe6925422
wanj <wanj@educ.college-montreal.qc.ca>
2022-10-13 10:21:33 -0400

    Version finale

--- jeu_devinette.py
+++ jeu_devinette.py
@@ -1,41 +1,50 @@
-# Programme de jeu de devinette entre 0 à 100
-#allosg4r
+"""
+Projet réalisé par Jia Lin Wan gr.403
+Programme qui permet à l'utilisateur de deviner un nombre entre deux bornes choisis
+"""
 
-from random import *
+from random import randint
 
 jouer = True
+rejouer = ""
+
+
+def jouer_a_nouveau():
+    """
+    Fonction qui permet de rejouer ou de quitter le programme lorsque l'utilisateur devine le nombre
+    """
+    global not_found, jouer, rejouer
+    rejouer = input("Voulez-vous rejouer? (y/n)")
+    not_found = False
+
+    if rejouer == "y":
+        jouer = True
+
+    elif rejouer == "n":
+        print("Au revoir")
+        jouer = False
+
 
 while jouer:
-    random_number = randint(0, 100)
+    borne_minimale = int(input("Borne minimale:"))
+    borne_maximale = int(input("Borne maximale:"))
+
+    random_number = randint(borne_minimale, borne_maximale)
     nb_essaie = 1
 
     not_found = True
     while not_found:
-
         try:
             user_guess = int(input("Quelle numéro devinez-vous?\n"))
-
-        except:
+        except ValueError:
             print("Veuillez ecrire un nombre s'il vous plaît.")
-
         else:
-
             if user_guess < random_number:
                 print("Vôtre numéro est trop petit")
                 nb_essaie += 1
-
             elif user_guess > random_number:
                 print("Vôtre numéro est trop grand.")
                 nb_essaie += 1
-
             elif user_guess == random_number:
                 print(f"Bravo! Vous avez devinez le numéro en {nb_essaie} essaies.")
-                rejouer = input("Voulez-vous rejouer? (y/n)")
-                not_found = False
-
-                if rejouer == "y":
-                    jouer = True
-
-                elif rejouer == "n":
-                    print("Au revoir")
-                    jouer = False
+                jouer_a_nouveau()
